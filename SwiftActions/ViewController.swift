0037bbe970e6dafc206ac13eaa7ec11e60b08279
franksco <franksco39@gmail.com>
2017-03-27 14:40:06 -0700

    added a UI switch and label as well as code to detect whether the switch is on or off while using the app and when the app first loads up as well.

--- SwiftActions/ViewController.swift
+++ SwiftActions/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var switchOutlet: UISwitch!
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if (switchOutlet.isOn) {
+            label.text = "The switch is on"
+        } else {
+            label.text = "The switch is off"
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func controlSwitch(_ sender: Any) {
+        
+        if (switchOutlet.isOn) {
+            label.text = "The switch is on"
+        } else {
+            label.text = "The switch is off"
+        }
+        
+    }
 
 }
 
